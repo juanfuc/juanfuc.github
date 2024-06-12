bd5f4295ba221e32c38981e6591a9d0dafdb818f
juanfuc <juanfuc@gmail.com>
2024-06-12 18:44:24 -0500

    Updates

---
+++ _app/immutable/nodes/2.C4RSCS0L.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.BaiVm2Fr.js";import{S as t,i as e}from"../chunks/index.DWRkMoV3.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
